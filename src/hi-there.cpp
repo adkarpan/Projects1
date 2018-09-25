45642ba503f425c2d53f13f122a80687d3a212ae
adkarpan <arpan.adk@gmail.com>
2018-09-25 05:24:03 -0500

    our first commit for the project.

---
+++ src/hi-there.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// hi-there.cpp
+// A simple application
+
+using namespace std;
+
+int main()
+{
+        cout << "Hi there! Wrlcome to Linux Academy!" << end1;
+        return 0;
+}
